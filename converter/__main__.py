e483769743089f832771d5e558f15109fd6abad5
Ding NING <ning.di@northeastern.edu>
2023-02-14 22:37:55 +1300

    Add files via upload

--- converter/__main__.py
+++ converter/__main__.py
@@ -12,13 +12,16 @@ arg_parser.add_argument('output_dirpath', type=str,
 arg_parser.add_argument('corr_threshold', type=str,
                         help='Node Feature Correlation Threshold')
 arg_parser.add_argument('is_directed', type=str,
-                        help="If Directed Graph: 'yes' or 'no'")
+                        help="If Directed Graphs: 'yes' or 'no'")
+arg_parser.add_argument('get_coords', type=str,
+                        help="If Output Coordinates: 'yes' or 'no'")
 args = arg_parser.parse_args()
 
 input_filepath = Path(args.input_filepath)
 output_dirpath = Path(args.output_dirpath)
 corr_threshold = Path(args.corr_threshold)
 is_directed = Path(args.is_directed)
+get_coords = Path(args.get_coords)
 
 # Read the input, process the input, and write the output.
-process_files(input_filepath, output_dirpath, corr_threshold, is_directed)
+process_files(input_filepath, output_dirpath, corr_threshold, is_directed, get_coords)
--- converter/process_files.py
+++ converter/process_files.py
@@ -7,7 +7,8 @@ from converter.output import *
 import numpy as np
 
 def process_files(input_filepath: str, output_dirpath: str, 
-                  corr_threshold: str, is_directed: str) -> None:
+                  corr_threshold: str, is_directed: str,
+                  get_coords: str) -> None:
   """
   Read the NC file that contains a spatiotemporal grid, and output the 
   converted graph in NPY files.
@@ -15,11 +16,12 @@ def process_files(input_filepath: str, output_dirpath: str,
   :param output_dirpath: the path to the output directory
   :param corr_threshold: the corrleation threshold to mask adjacencies
   :param is_directed: if the generated graph is directed
+  :param get_coords: if the node coordinates are output 
   """
   # Generate and save the node feature tensor.
   grid = input_grid(input_filepath)
   
-  # Get the coordinate tesnors.
+  # Get the coordinate tensor.
   coordinate_grid = get_coord_mat(input_filepath)
   grid_transposed = grid.transpose(1,2,0)
   grid_flattened = grid_transposed.reshape(grid_transposed.shape[0] * 
@@ -30,11 +32,13 @@ def process_files(input_filepath: str, output_dirpath: str,
   node_feats = drop_rows_with_nas(grid_flattened)
   output(node_feats, output_dirpath, 'node_feats')
   
-  # Also remove the corresponding nodes in the coorindate tensor.
-  land_indices = np.setdiff1d(np.arange(grid_flattened.shape[0]), np.where(
-                 (grid_flattened[:, np.newaxis] == node_feats).all(-1))[0])
-  coordinates_ocean = np.delete(coordinate_grid, land_indices, axis=0)
-  output(coordinates_ocean, output_dirpath, 'coords')
+  # Output the coordinate tensor (slow).
+  if str(get_coords) == 'yes':
+    # Also remove the corresponding nodes in the coorindate tensor.
+    land_indices = np.setdiff1d(np.arange(grid_flattened.shape[0]), np.where(
+                   (grid_flattened[:, np.newaxis] == node_feats).all(-1))[0])
+    coordinates_ocean = np.delete(coordinate_grid, land_indices, axis=0)
+    output(coordinates_ocean, output_dirpath, 'coords')
 
   # Convert the string into the float.
   corr_threshold = float(str(corr_threshold))
