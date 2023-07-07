761a93f4ee68445086ad9e572f33fc03ab5425a1
Ding NING <ning.di@northeastern.edu>
2023-07-07 19:32:58 +1200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='geostggc',
+    version='1.2',
+    packages=['converter'],
+    url='https://github.com/ding05/geo_st_grid_graph_converter',
+    license='MIT',
+    author='Ding Ning',
+    author_email='ding.ning@pg.canterbury.ac.nz',
+    description='A geo-spatiotemporal grid to graph converter'
+)
